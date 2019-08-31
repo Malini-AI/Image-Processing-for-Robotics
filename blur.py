57f60729af93db702ac51f695334952d68313b55
Malini <52051897+Malini-AI@users.noreply.github.com>
2019-08-31 14:04:17 -0400

    Update blur.py

--- blur.py
+++ blur.py
@@ -7,8 +7,6 @@ for imageName in glob.glob("....path/*.PNG"):
 v=1
 for image in imagelist:
     img = cv2.imread(image)  
-   
     blurImg = cv2.blur(img,(21,21))  
- 
     cv2.imwrite("blur"+str(v)+"_21.png", blurImg)  
     v+=1
