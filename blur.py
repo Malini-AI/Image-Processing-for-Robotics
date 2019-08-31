ae62a09c60b80dd5399cba7ac1b0087df551a43e
Malini <52051897+Malini-AI@users.noreply.github.com>
2019-08-31 14:03:52 -0400

    Create blur.py

---
+++ blur.py
@@ -0,0 +1,14 @@
+import cv2 
+
+imagelist=[]
+for imageName in glob.glob("....path/*.PNG"): 
+    imagelist.append(imageName)
+
+v=1
+for image in imagelist:
+    img = cv2.imread(image)  
+   
+    blurImg = cv2.blur(img,(21,21))  
+ 
+    cv2.imwrite("blur"+str(v)+"_21.png", blurImg)  
+    v+=1
